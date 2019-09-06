198f95e7ca825535ebdbbed50b7cce42666afa9f
Anup Jha <kul97095@gmail.com>
2019-09-07 00:09:44 +0530

    constructor example

--- Constructor_use .cpp
+++ Constructor_use .cpp
@@ -1,41 +1,43 @@
-//for Introduction to contructor
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <iostream>
+\\ c++ program to for constructor
 #include<bits/stdc++.h>
 
 using namespace std;
 
-struct student{
-    
-    int ID,year;
-    char name[20];
-    
-    student() //constructor for initilizizing the variable inside the struct
-    {
-        ID=6;
-        year=2019;
-        memset(name,0,sizeof(name));
-    }
-} student1;
 
-void printing( student st){
-    cout<<st.ID<<"\n"<<st.year;
-    cout<<st.name;
-    
-    
-}
+struct product{
+int price;
+char name[20];
+    product(){
+       price=10;
+          memset(name,0,sizeof(name));
+        }
+        product(int PRICE)
+        {
+         price=PRICE;
+        }
+        product(char st[]){
+            memset(name,0,sizeof(name));
+            strcpy(name,st);
+            
+        }
+
+
+
+
+
+};
+
+
 
 
 
 int main()
 {
-printing(student1);
-    return 0;
+    product X(15);
+    cout<<"\n";
+    product Y("anup");
+    cout<<X.price;
+    cout<<"\n";
+    cout<<Y.name;
+return 0;
 }
